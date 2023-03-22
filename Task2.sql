9589bbba4a0c22b18cfd0006c9f25e4e24f759ea
RootBoy <47785377+rootboy00@users.noreply.github.com>
2023-03-22 17:54:27 +0300

    Update Task2.sql

--- Task2.sql
+++ Task2.sql
@@ -5,6 +5,6 @@ CREATE TABLE temp_table_proc1
 as (select s.`name`, s.`cost` - COALESCE(d.discount, 0) as new_price
 	from services s
 	left join discounts d on s.service_id = d.service_id and d.timefrom < Idate and d.timeto > Idate
-	where s.timeto >= Idate
+	where s.timeto > Idate
 	);
 END
