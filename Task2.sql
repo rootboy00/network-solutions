68ee6f054cf71175c42af1abbd842a129c19aaac
RootBoy <47785377+rootboy00@users.noreply.github.com>
2023-03-22 17:48:06 +0300

    Update Task2.sql

--- Task2.sql
+++ Task2.sql
@@ -1,9 +1,10 @@
 CREATE PROCEDURE `proc1`( Idate DATE )
 BEGIN
-    drop table if EXISTS temp_table_proc1;
-    CREATE TABLE temp_table_proc1
-    as (select s.`name`, s.`cost` - COALESCE(d.discount, 0) as new_price
-        from services s
-        left join discounts d on s.service_id = d.service_id and d.timefrom <= Idate and d.timeto >= Idate
-        where s.timefrom <= Idate and s.timeto >= Idate);
+drop table if EXISTS temp_table_proc1;
+CREATE TABLE temp_table_proc1
+as (select s.`name`, s.`cost` - COALESCE(d.discount, 0) as new_price
+	from services s
+	left join discounts d on s.service_id = d.service_id and d.timefrom < Idate and d.timeto > Idate
+	where s.timeto >= Idate
+	);
 END
